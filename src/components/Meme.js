f4200741ebf9771ebf6ca5fae9f68f59a44d8868
AlexLozda32 <alexlozada.reyes@gmail.com>
2024-03-18 23:21:03 -0400

    Added meme API data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,23 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const getMemeImage = (e) => {
+    e.preventDefault(); // ! So page doesn't refresh
+    const randomNumber = Math.floor(
+      Math.random() * memesData.data.memes.length
+    );
+    const imageUrl = memesData.data.memes[randomNumber].url;
+
+    console.log(randomNumber, imageUrl);
+  };
+
   return (
     <main>
       <form>
         <input type="text" className="form--input" placeholder="Top text" />
         <input type="text" className="form--input" placeholder="Bottom text" />
-        <button type="submit" className="form--button">
+        <button type="submit" className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼️
         </button>
       </form>
